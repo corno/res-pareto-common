4a8ea5efe3c443f7ade449946e21ce234ba9e623
corno <corno.astn.a@schraverus.com>
2022-11-22 09:29:40 +0100

    .

--- pub/src/imp/public/aggregateStream.p.ts
+++ pub/src/imp/public/aggregateStream.p.ts
@@ -2,23 +2,23 @@ import * as pi from "pareto-core-internals"
 
 import * as api from "api-pareto-common"
 
-export const aggregateStream: api.FAggregateStream = (
-    $d
-) => {
-    return pi.wrapAsyncValueImp(
-        true,
-        (cb) => {
-            $d.connectToStream(
-                $d.createParser(
-                    cb,
-                    (v, $i) => {
-                        v._execute(($) => {
-                            $i($)
-                        })
-                    }
-                )
-            )
+// export const aggregateStream: api.FAggregateStream = (
+//     $d
+// ) => {
+//     return pi.wrapAsyncValueImp(
+//         true,
+//         (cb) => {
+//             $d.connectToStream(
+//                 $d.createParser(
+//                     cb,
+//                     (v, $i) => {
+//                         v._execute(($) => {
+//                             $i($)
+//                         })
+//                     }
+//                 )
+//             )
 
-        }
-    )
-}
+//         }
+//     )
+// }
